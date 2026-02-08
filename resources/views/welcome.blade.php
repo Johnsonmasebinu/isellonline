326884f49b8e7de6c3435096d594ae6e8c05b89d
ikobad3-cmy <ikobad3@gmail.com>
2026-02-08 14:48:44 +0100

    ggg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="facebook-domain-verification" content="doeysm2brw66ujj0ultu92ti32o8xy" />
     <title>ISellOnline - Your Online Marketplace</title>
     <link rel="icon" type="image/png" href="/assets/logo.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
